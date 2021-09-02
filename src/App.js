25d249db2b8b5bfe7ed01740a65b2c2aa7be96f0
Garnco <51217148+Garnco-lab@users.noreply.github.com>
2021-09-01 20:19:17 -0700

    final one

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Home from "./pages/Home";
 import Navbar from './components/Navbar';
+import Footer from "./components/Footer";
+import Menu from "./pages/Menu";
+import About from "./pages/About"
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -9,8 +13,12 @@ function App() {
       <Router>
         <Navbar/>
           <Switch>
-            <Route path="/" exact component={Home} />
+            <Route path = "/" exact component = {Home} />
+            <Route path = "/menu" exact component = {Menu}/>
+            <Route path = "/about" exact component = {About}/>
+            <Route path = "/contact" exact component = {Contact}/>
           </Switch>
+      <Footer/>
       </Router>
     </div>
   );
